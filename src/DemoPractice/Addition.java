ae813f79291ac27031672a78ccd7be07da1c32fc
Amol1011 <amol.sartape101@gmail.com>
2023-05-13 16:14:56 +0530

    First commit with Master branch

---
+++ src/DemoPractice/Addition.java
@@ -0,0 +1,22 @@
+package DemoPractice;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1,num2,total;
+		num1=10;
+		num2=20;
+		total=num1+num2;
+		if(total==30)
+		{
+			System.out.println("Addition functionality is working");
+		}
+		else
+		{
+			System.out.println("Addition functionality is not working");
+		}
+		
+
+	}
+
+}
